a471d0db996f355a27a09f5341bbc2df10940973
Brian Leung <hyleung111one@yahoo.com>
2022-01-18 09:02:49 +0800

    first commit

---
+++ src/main/java/ibf2021/assess2/Assess2Application.java
@@ -0,0 +1,13 @@
+package ibf2021.assess2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assess2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assess2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ibf2021/assess2/Assess2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2021.assess2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assess2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
